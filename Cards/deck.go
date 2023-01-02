8d737302c48ef99794ef0558e542e16dc8b38007
Dat Doan <42245488+SmileDragonfly@users.noreply.github.com>
2023-01-02 15:30:32 +0700

    Add function saveToFile: + Convert string slice to one string use strings.Write + Save string to file use ioutil.WriteFile

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Create new type of 'deck'
 // Which is a slice of strings
@@ -27,4 +31,12 @@ func newDeck() deck {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
 }
--- Cards/main.go
+++ Cards/main.go
@@ -1,14 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
-	fmt.Println("All cards:")
-	cards.print()
-	fmt.Println("Hand:")
-	hand.print()
-	fmt.Println("Remaining cards:")
-	remainingCards.print()
+	cards.saveToFile("my_cards")
 }
