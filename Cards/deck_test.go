4285a2810ec7ef6f0a84bed24080531287f0b064
Dat Doan <42245488+SmileDragonfly@users.noreply.github.com>
2023-01-04 12:10:05 +0700

    + Add file io testing

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -15,4 +18,15 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected first element is Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	d := newDeck()
+	d.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected length of deck is 16, but got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
