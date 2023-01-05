a025cb7cd88498dd39bf7eff7a6c781ccd1f5bfc
Dat Doan <42245488+SmileDragonfly@users.noreply.github.com>
2023-01-05 17:03:59 +0700

    + Add struct embedded + Use printf("%+v") to print field and value

--- Structs/main.go
+++ Structs/main.go
@@ -2,14 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	fmt.Println(alex)
+	Jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contact: contactInfo{
+			"JimParty@gmail.com",
+			94000,
+		},
+	}
+	// fmt.Println(Jim)
+	fmt.Printf("%+v", Jim) // value + field name
 }
