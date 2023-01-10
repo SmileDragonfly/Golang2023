5be4c7d325f6bc7c080863ea33e73c50d6197ef3
Dat Doan <42245488+SmileDragonfly@users.noreply.github.com>
2023-01-10 15:09:59 +0700

    + Add map manipulate

--- Map/main.go
+++ Map/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-	}
+	//colors := map[string]string{
+	//	"red":   "#ff0000",
+	//	"green": "#00ff00",
+	//}
+	colors := make(map[string]string)
+	colors["red"] = "#ff0000"
+	delete(colors, "red")
 	fmt.Println(colors)
 }
