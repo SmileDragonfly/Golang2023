46f1ac17e7e264d7dbbbd99e4b456fd20ba92402
Dat Doan <42245488+SmileDragonfly@users.noreply.github.com>
2023-01-10 15:19:07 +0700

    + Add iterate over a map

--- Map/main.go
+++ Map/main.go
@@ -3,12 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	//colors := map[string]string{
-	//	"red":   "#ff0000",
-	//	"green": "#00ff00",
-	//}
-	colors := make(map[string]string)
-	colors["red"] = "#ff0000"
-	delete(colors, "red")
-	fmt.Println(colors)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"white": "#ffffff",
+	}
+	//colors := make(map[string]string)
+	//colors["red"] = "#ff0000"
+	//delete(colors, "red")
+	//fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for key, value := range c {
+		fmt.Println("Hex code for color", key, "is", value)
+	}
 }
